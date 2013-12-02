1d0f40e7acbce06706a55d86f6e3ec7ddf8ab57a
Michael Binder <mb@thinkingstone.de>
2013-12-02 21:06:19 +0100

    do not launch firefox after site generation

--- site.sh
+++ site.sh
@@ -7,4 +7,4 @@ SITE=~/Downloads/dsa-documents/site
 rm -rf ${SITE}
 mkdir -p ${SITE}
 mvn site site:stage -DstagingDirectory=${SITE} -DtopSiteURL=http://www.thinkingstone.de
-firefox /home/mb/Downloads/dsa-documents/site/index.html &
+# firefox /home/mb/Downloads/dsa-documents/site/index.html &
